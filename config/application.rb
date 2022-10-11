315f7d872666f5a7f90ac97fe8eb826844d1e401
joao garcia <jiboiadamor@gmail.com>
2022-10-11 18:49:55 +0100

    added sidekiq

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ module RailsBackgroundJobs
       generate.test_framework :test_unit, fixture: false
     end
     # Initialize configuration defaults for originally generated Rails version.
+    config.active_job.queue_adapter = :sidekiq
     config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
